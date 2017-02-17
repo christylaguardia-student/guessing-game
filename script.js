18c6134c6408401b5292f96dc9684638e7946515
christylaguardia <christinelaguardia@gmail.com>
2017-02-16 18:53:25 -0800

    Major rewrite

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var userName = "";
 var nextQuestion = 1;
 var score = 0;
-var messageArray = [];
 
 // get user's name on page load
 getUserName();
@@ -34,94 +33,135 @@ function playGame() {
       break;
     case 4:
       question4();
-      getWinner();
-      nextQuestion = 0;
+      nextQuestion = 5;
       break;
+    case 5:
+      question5();
+      nextQuestion = 0;
     default:
       // reset the game
       nextQuestion = 1;
       score = 0;
-      messageArray = [];
       break;
   }
-
-  // show results in console
-  console.log("feedback: " + messageArray);
-  console.log("score: " + score);
   console.log("next question number: " + nextQuestion);
+}
 
-  // combine all the messages so far
-  var showMessages = "";
-  for (var i = 0; i < messageArray.length; i++) {
-    showMessages += messageArray[i];
-  }
+function askQuestion(promptText,defaultText) {
+  var userAnswer = prompt(promptText,defaultText);
+  return userAnswer;
+  console.log("Question: " + promptText);
+  console.log("Answer: " + userAnswer);
+}
 
-  // show messages on page
-  var questionAnswer = document.getElementById("gameResult");
-  questionAnswer.innerHTML = messageArray; // show everything in the array
+function displayQuestion(questionToDisplay) {
+  var questionElement = document.getElementById("gameQuestion");
+  questionElement.innerHTML = questionToDisplay;
+}
+
+function displayResult(answer,resultMessage) {
+  var displayAnswer = document.getElementById("userAnswer");
+  displayAnswer.innerHTML = userName + "'s answer: " + answer;
+  var displayResult = document.getElementById("gameResult");
+  displayResult.innerHTML = resultMessage;
+  var displayScore = document.getElementById("gameScore");
+  displayScore.innerHTML = score;
 }
 
 function question1() {
-  // ask the question
-  var userAnswer1 = prompt("Question 1:\nIs lemon an anagram of melon?", "yes or no");
-  console.log("question 1 answer: " + userAnswer1);
-  // check if they answered correctly
-  if (userAnswer1.toLowerCase() === 'yes' || userAnswer1.toLowerCase() === 'y') {
-    messageArray.push("Correct! Good job " + userName + "!");
+  var questionText = "Is lemon an anagram of melon?";
+  var userAnswer = askQuestion(questionText, "yes or no?");
+  var resultText = "";
+  if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() === 'y') {
+    resultText = "Correct! Good job " + userName + "!";
     score++;
   } else {
-    messageArray.push("Sorry " + userName + ", wrong answer! The letters in melon can be re-arranged to spell lemon.");
+    resultText = "Sorry " + userName + ", wrong answer! The letters in melon can be re-arranged to spell lemon.";
   }
+  displayQuestion(questionText);
+  displayResult(userAnswer,resultText);
+  document.getElementById("myBtn").value = "Next Question";
 }
 
 function question2() {
-  // ask the question
-  var userAnswer2 = prompt("Quesion 2:\nIs Mount Olympus located in the Alps?", "yes or no?");
-  console.log("question 2 answer: " + userAnswer2);
-  // check if they answered correctly
-  if (userAnswer2.toLowerCase() === 'no' || userAnswer2.toLowerCase() === 'n') {
-    messageArray.push("Right " + userName + "! Mount Olympus is in Greece. You sure know your mountains!");
+  var questionText = "Is Mount Olympus located in the Alps?";
+  var userAnswer = askQuestion(questionText, "yes or no?");
+  var resultText = "";
+  if (userAnswer.toLowerCase() === 'no' || userAnswer.toLowerCase() === 'n') {
+    resultText = "Right " + userName + "! Mount Olympus is in Greece. You sure know your mountains!";
     score++;
   } else {
-    messageArray.push("Nope, Mount Olympus is in Greece, not the Alps.");
+    resultText = "Nope, Mount Olympus is in Greece, not the Alps.";
   }
+  displayQuestion(questionText);
+  displayResult(userAnswer,resultText);
 }
 
 function question3() {
-  // ask the question
-  var userAnswer3 = prompt("Question 3:\nIs Mars the fourth planet from the Sun?", "yes or no?");
-  console.log("question 1 answer: " + userAnswer3);
-  // check if they answered correctly
-  if (userAnswer3.toLowerCase() === 'yes' || userAnswer3.toLowerCase() === 'y') {
-    messageArray.push("Bingo " + userName + "!");
+  var questionText = "Is Mars the fourth planet from the Sun?";
+  var userAnswer = askQuestion(questionText, "yes or no?");
+  var resultText = "";
+  if (userAnswer.toLowerCase() === 'yes' || userAnswer.toLowerCase() === 'y') {
+    resultText = "Bingo " + userName + "!";
     score++;
   } else {
-    messageArray.push("No " + userName + ". The forth planet from the Sun is Mars.");
+    resultText = "No " + userName + ". The forth planet from the Sun is Mars.";
   }
+  displayQuestion(questionText);
+  displayResult(userAnswer,resultText);
 }
 
 function question4() {
-  // ask the question
-  var userAnswer4 = prompt("Question 4:\nHow many fingers does Mickey Mouse have?", "enter number here");
-  console.log("question 4 answer: " + userAnswer4);
-  // check if they answered correctly
-  if (parseInt(userAnswer4) === 8) {
-    messageArray.push("Excelent " + userName + "!");
+  var questionText = "How many fingers does Mickey Mouse have?";
+  var userAnswer = askQuestion(questionText, "enter number here");
+  var resultText = "";
+  if (parseInt(userAnswer) === 8) {
+    resultText = "Excelent " + userName + "!";
     score++;
-  } else if (userAnswer4 < 8) {
-    messageArray.push("Actually, Mickey has 8 fingers total, not " + userAnswer4 + ". We're disappointed you didn't know that.");
-  } else if (userAnswer4 > 8) {
-    messageArray.push("Micky is a mouse. There's no way he could have " + userAnswer4 + " fingers. The right answer is 8.");
+  } else if (userAnswer < 8) {
+    resultText = "Actually, Mickey has 8 fingers total, not " + userAnswer + ". We're disappointed you didn't know that.";
+  } else if (userAnswer > 8) {
+    resultText = "Micky is a mouse. There's no way he could have " + userAnswer + " fingers. The right answer is 8.";
   } else {
-    messageArray.push("Wrong answer " + userName);
+    resultText = "Wrong answer " + userName;
+  }
+  displayQuestion(questionText);
+  displayResult(userAnswer,resultText);
+}
+
+function checkAnswer(answerToCheck, arrayToSearch) {
+  for (var i; i < arrayToSearch.length; i++) {
+    if (arrayToSearch[i] === answerToCheck) {
+      return true;
+    else {
+      return false;
+    }
   }
 }
 
-function getWinner() {
-  // find out if the user won
-  if (score === 4) {
-    messageArray.push("YOU WON! Congratulations " + userName + ", you got all 4 questions correct!");
-  } else {
-    messageArray.push("SORRY, YOU LOSE! Nice try " + userName + ", but you got " + score + " questions right and " + (4 - score) + " wrong.");
+function question5() {
+  var questionText = "Name one of Snow White's seven dwarfs."
+  var answerArray = ["Doc, Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey"];
+  var userAnswer = askQuestion(questionText,"dwarf name");
+  var resultText = "";
+  
+  if (checkAnswer(userAnswer, answerArray) === true) {
+      resultText = "Yes, " + userAnswer + " is one of the seven!";
+      score++;
+    } else {
+      resultText = "No, " + userAnswer + " is not one of the seven.";
+    }
   }
+
+  // find out if the user won
+  if (score === 5) {
+    resultText += "YOU WON! Congratulations " + userName + ", you got all 4 questions correct!";
+  } else {
+    resultText += "SORRY, YOU LOSE! Nice try " + userName + ", but you got " + score + " questions right and " + (4 - score) + " wrong.";
+  }
+
+  displayQuestion(questionText);
+  displayResult(userAnswer,resultText);
+
+  document.getElementById("myBtn").value = "New Game";
 }
